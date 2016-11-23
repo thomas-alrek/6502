454770f3b376a945fbc941aa8c528d37363ea055
Thomas Alrek <thomas@apility.no>
2016-11-24 00:46:52 +0100

    Added option for specifying dump range

--- 6502/debug/memory-dump.js
+++ 6502/debug/memory-dump.js
@@ -1,12 +1,27 @@
 "use strict";
 
-let memoryDump = function(){
+let memoryDump = function(_start, _end){
+
+	let start = 0;
+	let end = this.memory.length;
+
+	if(typeof _start !== 'undefined' && typeof _end !== 'undefined'){
+		start = _start;
+		if(_end > _start){
+			end = _end;
+		}
+		if(start < 0){
+			start = 0;
+			end = (_end - _start) + _end;
+		}
+	}
+
 	let padding = '';
 	for(let x = 0; x < this.memory.length.toString(16).length; x++){
 		padding += '0';
 	}
-	console.log('\nMemory dump ' + this.memory.length + ' byte(s)\n');
-	for(let i = 0; i < this.memory.length; i += 16){
+	console.log('\nMemory dump ' + (end - start) + ' byte(s)\n');
+	for(let i = start; i < end; i += 16){
 	let output = '0x' + (padding + i.toString(16)).substring(i.toString(16).length) + " | ";
 	let str = '';
 		for(let j = 0; j < 16; j++){
